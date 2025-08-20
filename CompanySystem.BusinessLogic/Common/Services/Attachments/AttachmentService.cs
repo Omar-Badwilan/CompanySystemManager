35198ac71cb26b5bcf8ce1f6d2f4954c0325dadc
Omar Ahmed <138378887+Omar-Badwilan@users.noreply.github.com>
2025-08-20 14:20:46 +0300

    Attachment Service

---
+++ CompanySystem.BusinessLogic/Common/Services/Attachments/AttachmentService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CompanySystem.BusinessLogic.Common.Services.Attachments
+{
+    internal class AttachmentService : IAttachmentService
+    {
+        private readonly List<string> _allowedExtensions = new() { ".png", ".jpg", ".jpeg" };
+        private const int _allowedMaxSize = 2_097_152;
+        public string? Upload(IFormFile file, string foldarName)
+        {
+            var extension = Path.GetExtension(file.FileName);
+
+            if (!_allowedExtensions.Contains(extension))
+                return null;
+            if (file.Length > _allowedMaxSize)
+                return null;
+
+            //var foldarPath = $"{Directory.GetCurrentDirectory}\\wwwroot\\files\\{foldarName}";
+
+            var foldarPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\files", foldarName);
+
+            if(!Directory.Exists(foldarPath))
+                Directory.CreateDirectory(foldarPath);
+
+            var fileName = $"{Guid.NewGuid()}-{extension}"; // UNIQUE
+
+            var filePath = Path.Combine(foldarPath, fileName); // File Location   
+
+            //streaming :data per time
+            // using for dispose
+
+            //using var fileStream = new FileStream(filePath, FileMode.Create);
+
+            using var fileStream = File.Create(filePath);
+
+            file.CopyTo(fileStream);
+
+            return fileName;
+        }
+
+        public bool Delete(string filePath)
+        {
+            if(File.Exists(filePath))
+            {
+                File.Delete(filePath);
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ CompanySystem.BusinessLogic/Common/Services/Attachments/IAttachmentService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CompanySystem.BusinessLogic.Common.Services.Attachments
+{
+    internal interface IAttachmentService
+    {
+        string? Upload(IFormFile file, string foldarName);
+
+        bool Delete(string filePath);
+    }
+}
