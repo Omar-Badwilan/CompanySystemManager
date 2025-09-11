57af12b6ee9527275ecab20d60f8874961422e11
Omar Ahmed <138378887+Omar-Badwilan@users.noreply.github.com>
2025-09-11 03:25:53 +0300

    Changed UpdateEmployeeAsync to fix the image setted into null
    You fetch the existing entity from the database, so EF Core is already tracking it.
    
    When you change its properties, EF knows exactly what changed → only those fields will be updated.
    
    Existing values (like Image) are preserved unless you explicitly overwrite them.
    
    Safer when some properties should not be lost.

--- CompanySystem.BusinessLogic/Services/Employees/EmployeeService.cs
+++ CompanySystem.BusinessLogic/Services/Employees/EmployeeService.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CompanySystem.BusinessLogic.Services.Employees
 {
-    public class EmployeeService(IUnitOfWork unitOfWork ,IAttachmentService attachmentService) : IEmployeeService
+    public class EmployeeService(IUnitOfWork unitOfWork, IAttachmentService attachmentService) : IEmployeeService
     {
         private readonly IUnitOfWork _unitOfWork = unitOfWork;
         private readonly IAttachmentService _attachmentService = attachmentService;
@@ -17,19 +17,19 @@ namespace CompanySystem.BusinessLogic.Services.Employees
         {
             var employees = await _unitOfWork.EmployeeRepository
                 .GetIQueryable()
-                .Where(E => !E.IsDeleted && (string.IsNullOrEmpty(search) || E.Name.ToLower().Contains(search.ToLower())) )
+                .Where(E => !E.IsDeleted && (string.IsNullOrEmpty(search) || E.Name.ToLower().Contains(search.ToLower())))
                 .Include(E => E.Department)
                 .Select(employee => new EmployeeDto()
                 {
-                Id = employee.Id,
-                Name = employee.Name,
-                Age = employee.Age,
-                IsActive = employee.IsActive,
-                Salary = employee.Salary,
-                Email = employee.Email,
-                Gender = employee.Gender.ToString(),
-                EmployeeType = employee.EmployeeType.ToString(),
-                Department = employee.Department != null ? employee.Department.Name : "No Department",
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    Age = employee.Age,
+                    IsActive = employee.IsActive,
+                    Salary = employee.Salary,
+                    Email = employee.Email,
+                    Gender = employee.Gender.ToString(),
+                    EmployeeType = employee.EmployeeType.ToString(),
+                    Department = employee.Department != null ? employee.Department.Name : "No Department",
                 }).ToListAsync();
 
             return employees;
@@ -83,7 +83,7 @@ namespace CompanySystem.BusinessLogic.Services.Employees
                 LastModifiedOn = DateTime.UtcNow,
             };
 
-            if(employeeDto.Image is not null)
+            if (employeeDto.Image is not null)
                 employee.Image = await _attachmentService.UploadFileAsync(employeeDto.Image, "images");
 
 
@@ -95,30 +95,28 @@ namespace CompanySystem.BusinessLogic.Services.Employees
 
         public async Task<int> UpdateEmployeeAsync(UpdateEmployeeDto employeeDto)
         {
-            var employee = new Employee()
-            {
-                Id = employeeDto.Id,
-                Name = employeeDto.Name,
-                Age = employeeDto.Age,
-                Address = employeeDto.Address,
-                IsActive = employeeDto.IsActive,
-                Salary = employeeDto.Salary,
-                Email = employeeDto.Email,
-                PhoneNumber = employeeDto.PhoneNumber,
-                HiringDate = employeeDto.HiringDate,
-                Gender = employeeDto.Gender,
-                EmployeeType = employeeDto.EmployeeType,
-                DepartmentId = employeeDto.DepartmentId,
-                CreatedBy = 1,
-                LastModifiedBy = 1,
-                LastModifiedOn = DateTime.UtcNow,
-            };
+            var employee = await _unitOfWork.EmployeeRepository.GetByIdAsync(employeeDto.Id);
+            if (employee is null)
+                return 0;
+            employee.Name = employeeDto.Name;
+            employee.Age = employeeDto.Age;
+            employee.Address = employeeDto.Address;
+            employee.IsActive = employeeDto.IsActive;
+            employee.Salary = employeeDto.Salary;
+            employee.Email = employeeDto.Email;
+            employee.PhoneNumber = employeeDto.PhoneNumber;
+            employee.HiringDate = employeeDto.HiringDate;
+            employee.Gender = employeeDto.Gender;
+            employee.EmployeeType = employeeDto.EmployeeType;
+            employee.DepartmentId = employeeDto.DepartmentId;
+            employee.LastModifiedBy = 1;
+            employee.LastModifiedOn = DateTime.UtcNow;
 
-               //  Only update image if a new one was uploaded
-    if (employeeDto.Image is not null)
-    {
-        employee.Image = await _attachmentService.UploadFileAsync(employeeDto.Image, "images");
-    }
+            //  Only update image if a new one was uploaded
+            if (employeeDto.Image is not null)
+            {
+                employee.Image = await _attachmentService.UploadFileAsync(employeeDto.Image, "images");
+            }
             _unitOfWork.EmployeeRepository.Update(employee);
 
             return await _unitOfWork.CompleteAsync();
@@ -131,7 +129,7 @@ namespace CompanySystem.BusinessLogic.Services.Employees
             var employee = await employeeRepo.GetByIdAsync(id);
 
             if (employee is { })
-                 employeeRepo.Delete(employee);
+                employeeRepo.Delete(employee);
 
             return await _unitOfWork.CompleteAsync() > 0;
 
